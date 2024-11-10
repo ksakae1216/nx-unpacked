4532326e8144ab447d6a27fc3c5bbb2f45b0cf78
kotaro_sakae <ksakae1216@gmail.com>
2024-11-10 10:29:20 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, effect, signal, untracked } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, MatSelectModule, CommonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'nx-unpacked';
+  loginMethods = ['Email', 'Google', 'GitHub'];
+
+  selectedLoginMethod = signal(this.loginMethods[0]);
+
+  constructor(private snackBar: MatSnackBar) {
+    const count = signal(0);
+    effect(
+      () => {
+        const loginMethod = this.selectedLoginMethod();
+        const currentCount = untracked(() => count()); // countの変更ではeffectを発火させたくない
+        this.showToast(
+          `${currentCount}回目: ログイン方法が${loginMethod}に変更されました`
+        );
+        count.set(currentCount + 1);
+      },
+      { allowSignalWrites: true }
+    );
+  }
+
+  private showToast(message: string) {
+    this.snackBar.open(message, '閉じる', {
+      duration: 3000,
+    });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,6 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
+import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
-  ],
+  providers: [provideAnimations()],
 };
